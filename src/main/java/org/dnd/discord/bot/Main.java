a23fdf392e8e67fe68661cf38fd20353a5a339f9
grieverbs <grieverbs@gmail.com>
2018-11-04 23:54:30 -0700

    Ask Players every noon on monday if we're playing this week?

--- src/main/java/org/dnd/discord/bot/Main.java
+++ src/main/java/org/dnd/discord/bot/Main.java
@@ -2,39 +2,58 @@ package org.dnd.discord.bot;
 
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import org.dnd.discord.utility.Configure;
 
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main extends ListenerAdapter {
     private static Configure configure = new Configure();
-
-    public Main() {
-    }
+    private static JDABuilder jdaBuilder;
+    private static String DUNGEONS_AND_DRAGONS_CHANNEL = "dungeons_and_dragons";
+    private static String DUNGEONS_AND_DRAGONS_ROLE = "players";
+    private static int NOON = 12;
+    private static int MONDAY = 1;
+//    private static String TEST_CHANNEL = "general";
+//    private static String TEST_ROLE = "super friends";
 
     public static void main(String[] args) throws LoginException {
-        var jdaBuilder = new JDABuilder(AccountType.BOT);
+        jdaBuilder = new JDABuilder(AccountType.BOT);
         try {
             jdaBuilder.setToken(configure.getBotToken());
             jdaBuilder.addEventListener(new Main());
             jdaBuilder.buildAsync();
-        } catch(IOException exception) {
+        } catch (IOException exception) {
             System.out.println(exception.getMessage());
             System.exit(0);
         }
     }
 
-//    public void ListenerAda() {
-//        event.getGuild().getRoles().stream().filter(x -> x.getName().contains("players")).findFirst().orElse(null);
-//    }
-
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getMessage().getContentRaw().toLowerCase().contains("who is the best player")) {
-            event.getChannel().sendMessageFormat("<@%s> is!",event.getGuild().getOwner().getUser().getId()).queue();
+            event.getChannel().sendMessageFormat("<@%s> is!", event.getGuild().getOwner().getUser().getId()).queue();
+        }
+    }
+
+    @Override
+    public void onReady(ReadyEvent event) {
+        var myChannel = event.getJDA().getTextChannels().stream().filter(x -> x.getName().toLowerCase()
+                .contains(DUNGEONS_AND_DRAGONS_CHANNEL)).findFirst().orElse(null);
+        var myRole = event.getJDA().getRoles().stream().filter(x -> x.getName().toLowerCase()
+                .contains(DUNGEONS_AND_DRAGONS_ROLE)).findFirst().orElse(null);
+        if (myChannel != null && myRole != null) {
+            var execService = Executors.newScheduledThreadPool(1);
+            execService.scheduleAtFixedRate(() -> {
+                if (LocalDateTime.now().getHour() == NOON && LocalDateTime.now().getDayOfWeek().getValue() == MONDAY)
+                    myChannel.sendMessageFormat("Hey %s, are we continuing our quest this week?", myRole).queue();
+            }, 0, 1, TimeUnit.HOURS);
         }
     }
 }
